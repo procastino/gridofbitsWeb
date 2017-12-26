11d1be0feb14e30e1b2760a26952f4815b0d1d94
procastino <tuchodefresa@gmail.com>
2017-12-26 21:30:12 +0100

    basic structure

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ var resIdHorizontal=["resH1","resH2","resH3","resH4"];
 var resIdVertical=["resV1","resV2","resV3","resV4"];
 //list for storing results [horizontal][vertical]
 var results=[[0,0,0,0],[0,0,0,0]];
+//list for storing player results [horizontal][vertical]
+var playerResults=[[0,0,0,0],[0,0,0,0]];
 
 function populate(){
   //populating the grid with 0 and 1
@@ -21,8 +23,7 @@ function populate(){
       var bit=Math.floor(Math.random()*2);
       bits[i][j]=bit;
       var button=buttonsId[i][j];
-      document.getElementById(button).innerHTML=bit;
-
+      //document.getElementById(button).innerHTML=bit;
     }
   }
   //populating the horizontal results
@@ -48,10 +49,31 @@ function populate(){
           else {
             $(this).html('0');
           }
+          //checking horizontal values
+          for (i=0;i<4;i++){
+            for (j=0;j<4;j++){
+              playerResults[0][i] = playerResults[0][i]+decValues[j]*document.getElementById(buttonsId[i][j]).innerHTML;
+              playerResults[1][i]=
+              playerResults[1][i]+decValues[j]*document.getElementById(buttonsId[j][i]).innerHTML;
+              }
+            if (playerResults[0][i]==results[0][i]){
+                $('#'+resIdHorizontal[i]).css("background-color", "orange");
+                }
+                else {
+                  $('#'+resIdHorizontal[i]).css("background-color", "white");
+                }
+            if (playerResults[1][i]==results[1][i]){
+                $('#'+resIdVertical[i]).css("background-color", "orange");
+                }
+                else {
+                  $('#'+resIdVertical[i]).css("background-color", "white");
+                }
+              }
+            playerResults=[[0,0,0,0],[0,0,0,0]];
+
       });
   });
 }
-
 
 function showGrid(){
   for (i=0;i<4;i++){

01dc9c56ae6a9f8cbd01488d36407382b9ea359a
procastino <tuchodefresa@gmail.com>
2017-12-28 14:52:44 +0100

    re-styled code

--- game.js
+++ game.js
@@ -8,12 +8,13 @@ var buttonsId =[
   ["4_1","4_2","4_3","4_4"]
 ];
 //list of id's for the results
-var resIdHorizontal=["resH1","resH2","resH3","resH4"];
-var resIdVertical=["resV1","resV2","resV3","resV4"];
+var resId=[["resH1","resH2","resH3","resH4"],["resV1","resV2","resV3","resV4"]];
 //list for storing results [horizontal][vertical]
 var results=[[0,0,0,0],[0,0,0,0]];
 //list for storing player results [horizontal][vertical]
 var playerResults=[[0,0,0,0],[0,0,0,0]];
+//number of correct lines and rows to check equality
+var numCorrect=0;
 
 function populate(){
   //populating the grid with 0 and 1
@@ -23,24 +24,23 @@ function populate(){
       var bit=Math.floor(Math.random()*2);
       bits[i][j]=bit;
       var button=buttonsId[i][j];
-      //document.getElementById(button).innerHTML=bit;
+      }
     }
-  }
-  //populating the horizontal results
-  for (j=0;j<4;j++){
-    for (i=0;i<4;i++){
-      results[0][j] = results[0][j]+decValues[i]*bits[j][i];
+  //populating the results
+  for (k=0;k<2;k++){
+    for (j=0;j<4;j++){
+      for (i=0;i<4;i++){
+        if(k==0){
+          results[k][j] = results[k][j]+decValues[i]*bits[j][i];
+          }
+        else {
+          results[k][j] = results[k][j]+decValues[i]*bits[i][j];
+          }
+        }
+      document.getElementById(resId[k][j]).innerHTML=results[k][j];
       }
-      document.getElementById(resIdHorizontal[j]).innerHTML=results[0][j];
-  }
-  //populating the vertical results
-  for (j=0;j<4;j++){
-    for (i=0;i<4;i++){
-      results[1][j] = results[1][j]+decValues[i]*bits[i][j];
-      }
-    document.getElementById(resIdVertical[j]).innerHTML=results[1][j];
-  }
-  //button management
+    }
+  //button management for each bit
   $(document).ready(function(){
       $("button").click(function(){
           if ($(this).html()==0){
@@ -49,34 +49,41 @@ function populate(){
           else {
             $(this).html('0');
           }
-          //checking horizontal values
-          for (i=0;i<4;i++){
-            for (j=0;j<4;j++){
-              playerResults[0][i] = playerResults[0][i]+decValues[j]*document.getElementById(buttonsId[i][j]).innerHTML;
-              playerResults[1][i]=
-              playerResults[1][i]+decValues[j]*document.getElementById(buttonsId[j][i]).innerHTML;
-              }
-            if (playerResults[0][i]==results[0][i]){
-                $('#'+resIdHorizontal[i]).css("background-color", "orange");
-                }
+          //checking player results
+          for (k=0;k<2;k++){
+            for (i=0;i<4;i++){
+              for (j=0;j<4;j++){
+                if (k==0){
+                playerResults[k][i] = playerResults[k][i]+decValues[j]*document.getElementById(buttonsId[i][j]).innerHTML;
+                  }
                 else {
-                  $('#'+resIdHorizontal[i]).css("background-color", "white");
+                  playerResults[k][i] = playerResults[k][i]+decValues[j]*document.getElementById(buttonsId[j][i]).innerHTML;
+                  }
                 }
-            if (playerResults[1][i]==results[1][i]){
-                $('#'+resIdVertical[i]).css("background-color", "orange");
+              if (playerResults[k][i]==results[k][i]){
+                $('#'+resId[k][i]).css("background-color", "orange");
                 }
-                else {
-                  $('#'+resIdVertical[i]).css("background-color", "white");
+              else {
+                $('#'+resId[k][i]).css("background-color", "white");
                 }
               }
-            playerResults=[[0,0,0,0],[0,0,0,0]];
-
+            }
+// THIS DOESN'T WORK!! LOOK FOR SOMETHING THAT COMPARES THE TWO ARRAYS
+            // for (i=0;i<2;i++){
+            //   for (j=0;j<4;j++){
+            //     if (results[i][j]==playerResults[i][j]){
+            //       numCorrect++;
+            //       if (numCorrect==8){
+            //         document.getElementById('final').innerHTML="Conseguíchelo!";
+            //         }
+            //       else {
+            //         console.log(numCorrect);
+            //         numCorrect=0;
+            //         }
+            //       }
+            //     }
+            //   }
+              playerResults=[[0,0,0,0],[0,0,0,0]];
       });
   });
-}
-
-function showGrid(){
-  for (i=0;i<4;i++){
-
-  }
 }
